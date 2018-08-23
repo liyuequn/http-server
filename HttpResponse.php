1def2d063c8d761c5fcfdf396a8949586238e28f
李岳群 <liyuequn@51shebao.com>
2018-08-23 18:21:21 +0800

    Initial commit

---
+++ HttpResponse.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: liyuequn
+ * Date: 2018/8/23
+ * Time: 上午11:57
+ */
+
+class HttpResponses
+{
+    Const STATUS = [
+        '200'=>'ok',
+
+        '301'=>'Moved Permanently',
+        '302'=>'Found',
+
+        '400'=>'Bad Request',
+        '401'=>'Unauthorized',
+        '403'=>'Forbidden',
+        '500'=>'Internal Server Error',
+        '502'=>'Bad GateWay'
+    ];
+
+    Const OPTIONS = [
+        "Server" => "phpHttpServer",
+        "Content-Type" => "text/html; charset=UTF-8",
+    ];
+
+
+
+    protected $header;
+
+    protected $options;
+
+    protected $status;
+
+    protected $content;
+
+    public function __construct($status = 200)
+    {
+
+        $this->header = "HTTP/1.1 {$status} ".self::STATUS[$status]." \r\n";
+        $this->options = self::OPTIONS;
+    }
+
+    public function setHeader(string $key, string $value)
+    {
+        $this->options[$key] = $value;
+    }
+
+    public function response(string $content,$status = 200)
+    {
+        $text = "HTTP/1.1 {$status} ".self::STATUS[$status]."\r\n";
+
+        foreach ($this->options as $key => $option)
+        {
+            $text .= $key .": ".$option."\r\n";
+        }
+        $text .= "Content-Length: ".strlen($content)."\r\n";
+
+        $this->header = $text;
+
+        $this->content = $content;
+
+        return $this->header."\r\n".$this->content;
+
+    }
+}
---
+++ index.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: liyuequn
+ * Date: 2018/8/23
+ * Time: 下午3:52
+ */
+
+$content = '<h1 style="
+text-align: center;
+color:aqua;
+margin-top: 200px;
+font-size:81px;
+font-weight: bold;
+background: grey;
+">
+Hello PhpWebServer   !!</h1>';
+$address = '0.0.0.0';
+$port = 8888;
+$length = 1024;
+
+include "./HttpResponse.php";
+
+$http = new HttpResponses();
+
+$sock = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+//can reuse port
+socket_set_option($sock, SOL_SOCKET, SO_REUSEADDR, 1);
+
+socket_bind($sock,$address,$port);
+
+socket_listen($sock,128);
+
+$clients [] = $sock;
+
+while (true)
+{
+    $reads = $clients;
+    if(socket_select($reads,$writes,$except,NULL) < 1) {
+        continue;
+    }
+
+    if(in_array($sock,$reads))
+    {
+        $writes[] = $sockAccepted = socket_accept($sock);
+        $clients[] = $sockAccepted;
+        $key = array_search($sock,$reads);
+        unset($reads[$key]);
+
+    }
+
+    foreach ($reads as $read)
+    {
+        $data = @socket_read($read,$length);
+
+        if($data === false)
+        {
+            unset($clients[array_search($read,$clients)]);
+            unset($reads[array_search($read,$reads)]);
+            continue;
+        }
+        $data = $http->response($content);
+        socket_write($read,$data);
+    }
+
+}
+
+socket_close($sock);
